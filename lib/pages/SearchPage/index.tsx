e6835834fb7f2c7319e574556ea74fe36cae1e06
Kyzyl-ool Kezhik <kyzyloolk@mail.ru>
2020-04-05 18:01:17 +0300

    fix debounce

--- lib/pages/SearchPage/index.tsx
+++ lib/pages/SearchPage/index.tsx
@@ -1,24 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { HeroCard, IHeroCard } from 'components/HeroCard';
-import { getHero, heroes, heroesSearch, getHeroes } from '../../mocks/heroes';
+import { heroesSearch, getHeroes } from '../../mocks/heroes';
 import { Input } from 'semantic-ui-react';
 import './SearchPage.scss';
 import _debounce from 'lodash/debounce';
 
 export const HeroesPage: React.FC = props => {
-  const [searchString, setSearchString] = useState<string>('');
   const [foundHeroes, setFoundHeroes] = useState<IHeroCard[]>([]);
 
-  const fetchData = _debounce(async () => {
+  const fetchData = _debounce(async (searchString: string) => {
     if (searchString.length > 0) {
       const res: IHeroCard[] = await heroesSearch(searchString);
       setFoundHeroes(res);
     }
   }, 300);
-
-  useEffect(() => {
-    fetchData();
-  }, [searchString]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -32,15 +27,11 @@ export const HeroesPage: React.FC = props => {
   return (
     <div>
       <h3>Поиск по выборке:</h3>
-      <Input placeholder={'Фамилия, Имя, Отчество...'} onChange={event => setSearchString(event.target.value)} fluid />
+      <Input placeholder={'Фамилия, Имя, Отчество...'} onChange={event => fetchData(event.target.value)} fluid />
       <div className={'search-page__search-result'}>
-        {foundHeroes
-          .filter(value =>
-            `${value.lastName} ${value.firstName} ${value.patronym}`.toUpperCase().includes(searchString.toUpperCase())
-          )
-          .map((value, index) => (
-            <HeroCard key={index} {...value} />
-          ))}
+        {foundHeroes.map((value, index) => (
+          <HeroCard key={index} {...value} />
+        ))}
       </div>
     </div>
   );
